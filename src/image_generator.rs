912ba18cc4dc2086a546622c0b7e3b4c240d3e72
AndrewChe <Chernjakov2@gmail.com>
2023-07-25 00:27:27 +0300

    Add base functions and scripting language

--- src/image_generator.rs
+++ src/image_generator.rs
@@ -1,5 +1,10 @@
 use std::path::PathBuf;
-use image::{ImageBuffer, RgbaImage};
+use std::sync::Arc;
+use image::RgbaImage;
+use rune::{Context, Source, Sources};
+use tokio::fs::File;
+use tokio::io;
+
 
 pub struct WallpaperBuilder {
     current_image: RgbaImage,
@@ -9,6 +14,12 @@ pub struct Wallpaper {
     image: RgbaImage,
 }
 
+pub struct Generator {
+    wallpaper: WallpaperBuilder,
+    context: Context,
+    sources: Sources,
+}
+
 impl WallpaperBuilder {
     pub fn new() -> Self {
         Self {
@@ -16,9 +27,28 @@ impl WallpaperBuilder {
         }
     }
 
+    pub fn solid(mut self, color: &[u8; 3], width: u32, height: u32) -> Self {
+        let mut im = RgbaImage::new(width, height);
+        for pixel in im.pixels_mut() {
+            pixel.0[0] = color[0];
+            pixel.0[1] = color[1];
+            pixel.0[2] = color[2];
+            pixel.0[3] = 255;
+        }
+        self.current_image = im;
+        self
+    }
+
     pub fn load_image(mut self, path: &PathBuf) -> Self {
         let im = image::open(path).expect("Can't load image");
         self.current_image = im.into_rgba8();
+        self
+    }
+
+    pub fn append_image(mut self, path: &PathBuf) -> Self {
+        let im = image::open(path).expect("Can't load image");
+        let im = im.into_rgba8();
+        image::imageops::overlay(&mut self.current_image, &im, 0, 0);
         self
     }
 
@@ -30,7 +60,26 @@ impl WallpaperBuilder {
 }
 
 impl Wallpaper {
-    pub fn save(path: &PathBuf) {
+    pub fn save(&self, path: &PathBuf) {
+        self.image.save(path).expect("Can't save image");
+    }
+}
+
+impl Generator {
+    pub async fn new(path: &PathBuf) -> Self {
+        let wallpaper = WallpaperBuilder::new();
+        let context = Context::with_default_modules().expect("Can't create context");
+        let mut sources = Sources::new();
+        sources.insert(Source::from_path(path).expect("Can't load script"));
+
+        Self {
+            wallpaper,
+            context,
+            sources,
+        }
+    }
+
+    pub async fn run_script(&mut self) {
         todo!()
     }
 }
