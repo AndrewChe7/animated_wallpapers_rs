95cb433d13b04556a435bca34f0e45e63fe67f23
AndrewChe <Chernjakov2@gmail.com>
2023-07-25 17:07:21 +0300

    Removed testing crate

--- src/cli.rs
+++
@@ -1,8 +0,0 @@
-use std::path::PathBuf;
-use animated_wallpapers_rs::image_generator::Generator;
-
-#[tokio::main]
-async fn main() {
-    let mut generator = Generator::new(PathBuf::from("./test.rn"));
-    generator.update().await;
-}
